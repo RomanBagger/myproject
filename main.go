d2fcedcaa337e12b5acba05d9541a65e5589861d
Raman Markau <romanmarkov039@gmail.com>
2024-06-04 14:28:14 +0200

    fr-1

--- main.go
+++ main.go
@@ -1,25 +1,26 @@
-// main.go
 package main
 
 import (
 	"log"
 	"net/http"
+
 	"github.com/RomanBagger/myproject.git/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/register", handlers.Register).Methods("POST")
 	router.HandleFunc("/login", handlers.Login).Methods("POST")
+
 	log.Println("Starting server on :10000")
 	if err := http.ListenAndServe(":10000", router); err != nil {
-		log.Fatalf("Could not start server: %s\n", err)
-	}
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Fatalf("Could not start server: %v", err)
 	}
 }
